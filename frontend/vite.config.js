fa938d294a256904f36f217bbf43398ad3a7c789
Ollie Taylor <13766232+OllieJT@users.noreply.github.com>
2023-05-15 10:17:30 +0100

    adds vitest unit testing

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,6 @@
-import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 import preprocess from 'svelte-preprocess';
+import { defineConfig } from 'vitest/config';
 
 // https://vitejs.dev/config/
 export default defineConfig({
